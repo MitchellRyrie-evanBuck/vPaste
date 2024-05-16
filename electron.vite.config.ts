0a501f8e9a90cb8baa0e5b300df7c9352ef72928
liuxiaowen <liuxiaowen66621@gmail.com>
2024-05-16 20:34:07 +0800

    fix: VPaste ugupdate

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -3,9 +3,8 @@ import { defineConfig, externalizeDepsPlugin, swcPlugin } from 'electron-vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-
   main: {
-    plugins: [externalizeDepsPlugin(), swcPlugin(), ],
+    plugins: [externalizeDepsPlugin(), swcPlugin()],
     build: {
       rollupOptions: {
         input: {
@@ -15,7 +14,7 @@ export default defineConfig({
     }
   },
   preload: {
-    plugins: [externalizeDepsPlugin(),],
+    plugins: [externalizeDepsPlugin()],
     build: {
       rollupOptions: {
         input: {
@@ -27,14 +26,16 @@ export default defineConfig({
   renderer: {
     resolve: {
       alias: {
-        '@renderer': resolve('src/renderer/src')
+        '@renderer': resolve('src/renderer/src'),
+        '@preload': resolve('src/preload'),
+        '@main': resolve('src/main')
       }
     },
     plugins: [react()],
     build: {
       rollupOptions: {
         input: {
-          index: resolve(__dirname, 'index.html')
+          index: resolve(__dirname, 'src/renderer/index.html')
         }
       }
     }
