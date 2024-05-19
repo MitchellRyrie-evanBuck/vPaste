0a7dfb996999b72bb1376079d1763bc2c06002bb
liuxiaowen <liuxiaowen66621@gmail.com>
2024-05-19 19:16:26 +0800

    init whenReady

--- src/main/index.ts
+++ src/main/index.ts
@@ -27,8 +27,7 @@ function createWindow(): void {
     return { action: 'deny' }
   })
 
-  // HMR for renderer base on electron-vite cli.
-  // Load the remote URL for development or the local html file for production.
+
   if (is.dev && process.env['ELECTRON_RENDERER_URL']) {
     mainWindow.loadURL(process.env['ELECTRON_RENDERER_URL'])
   } else {
@@ -37,9 +36,7 @@ function createWindow(): void {
 }
 
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
+
 app.whenReady().then(() => {
   // Set app user model id for windows
   electronApp.setAppUserModelId('com.electron')
